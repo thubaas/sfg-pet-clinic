e7d61059f59ec6742429d12958ac87cbd4df0242
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-06-21 14:23:45 +0100

    Added Vet lisst and updated controller. closes #15

--- src/main/java/com/thubaas/sfgpetclinic/controllers/VetController.java
+++ src/main/java/com/thubaas/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.thubaas.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thubaas.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVests() {
+	public String listVests(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
