fe828069eac483bdc952af44044cb5e28c3c223f
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-06-21 12:14:02 +0100

    added owners list and updated controller. closes #16

--- src/main/java/com/thubaas/sfgpetclinic/controllers/OwnerController.java
+++ src/main/java/com/thubaas/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
+
 package com.thubaas.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thubaas.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
 
 	@RequestMapping({ "", "/", "/index", "/index.html" })
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 
