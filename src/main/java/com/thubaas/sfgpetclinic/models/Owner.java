a14eb83bef81b5c0c5fbe1bf57a00f57ad9e97ee
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-06-27 20:05:34 +0100

    added Visit entity, updated Owner with Pet relationship. closes #26

--- src/main/java/com/thubaas/sfgpetclinic/models/Owner.java
+++ src/main/java/com/thubaas/sfgpetclinic/models/Owner.java
@@ -1,7 +1,21 @@
 package com.thubaas.sfgpetclinic.models;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 
 }
