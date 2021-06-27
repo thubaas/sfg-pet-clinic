b75894b3028f2dd4f3673efb572f2b0ed4c2d59b
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-06-27 20:19:13 +0100

    created specialties, added to vet. closes #27

---
+++ src/main/java/com/thubaas/sfgpetclinic/models/Specialty.java
@@ -0,0 +1,18 @@
+package com.thubaas.sfgpetclinic.models;
+
+public class Specialty extends BaseEntity {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
--- src/main/java/com/thubaas/sfgpetclinic/models/Vet.java
+++ src/main/java/com/thubaas/sfgpetclinic/models/Vet.java
@@ -1,7 +1,19 @@
 package com.thubaas.sfgpetclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
 	private static final long serialVersionUID = 1L;
+	
+	private Set<Specialty> specialties;
 
+	public Set<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		this.specialties = specialties;
+	}
+	
 }
