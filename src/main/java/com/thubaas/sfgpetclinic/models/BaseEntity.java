7511586602caa11ebb6c7777773442dcdd46803e
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-06-30 14:45:26 +0100

    updated base entity with JPA annotations. closes #35

--- src/main/java/com/thubaas/sfgpetclinic/models/BaseEntity.java
+++ src/main/java/com/thubaas/sfgpetclinic/models/BaseEntity.java
@@ -2,10 +2,18 @@ package com.thubaas.sfgpetclinic.models;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
