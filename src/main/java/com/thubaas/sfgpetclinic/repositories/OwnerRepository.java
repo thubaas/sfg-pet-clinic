a52fd3d4568459a44156143fe3ebc0f73e4d55f7
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-06-30 21:29:10 +0100

    added spring data jpa service for owners. closes #40

--- src/main/java/com/thubaas/sfgpetclinic/repositories/OwnerRepository.java
+++ src/main/java/com/thubaas/sfgpetclinic/repositories/OwnerRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.thubaas.sfgpetclinic.models.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long>{
+	
+	Owner findByLastName(String lastName);
 
 }
---
+++ src/main/java/com/thubaas/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,65 @@
+package com.thubaas.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.thubaas.sfgpetclinic.models.Owner;
+import com.thubaas.sfgpetclinic.repositories.OwnerRepository;
+import com.thubaas.sfgpetclinic.repositories.PetRepository;
+import com.thubaas.sfgpetclinic.repositories.PetTypeRepository;
+import com.thubaas.sfgpetclinic.services.OwnerService;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+	
+	private final OwnerRepository ownerRepository;
+	private final PetRepository petRepository;
+	private final PetTypeRepository petTypeRepository;
+	
+
+	public OwnerSDJpaService(OwnerRepository ownerRepository, 
+			PetRepository petRepository, PetTypeRepository petTypeRepository) {
+		super();
+		this.ownerRepository = ownerRepository;
+		this.petRepository = petRepository;
+		this.petTypeRepository = petTypeRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> owners = new HashSet<>();
+		ownerRepository.findAll().forEach(owners::add);
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return ownerRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner owner) {
+		return ownerRepository.save(owner);
+	}
+
+	@Override
+	public void delete(Owner owner) {
+		ownerRepository.delete(owner);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownerRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return ownerRepository.findByLastName(lastName);
+	}
+
+}
