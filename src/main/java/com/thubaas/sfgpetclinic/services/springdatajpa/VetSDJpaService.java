49e11fc6fe80d3de8143795e187b3b9531e47238
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-07-01 05:03:17 +0100

    added spring data jpa service for vets. closes #41

---
+++ src/main/java/com/thubaas/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,51 @@
+package com.thubaas.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.thubaas.sfgpetclinic.models.Vet;
+import com.thubaas.sfgpetclinic.repositories.VetRepository;
+import com.thubaas.sfgpetclinic.services.VetService;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+	
+	private final VetRepository vetRepository;
+	
+	public VetSDJpaService(VetRepository vetRepository) {
+		super();
+		this.vetRepository = vetRepository;
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+		Set<Vet> vets = new HashSet<>();
+		vetRepository.findAll().forEach(vets::add);
+		return vets;
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Vet save(Vet vet) {
+		return vetRepository.save(vet);
+	}
+
+	@Override
+	public void delete(Vet vet) {
+		vetRepository.delete(vet);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);		
+	}
+
+}
