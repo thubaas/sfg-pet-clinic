58b15f118843a5d5efeca5d4a311e64869d2f595
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-07-01 10:05:33 +0100

    Implemented Map impl for Visit Service. closes #46

---
+++ src/main/java/com/thubaas/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.thubaas.sfgpetclinic.services;
+
+import com.thubaas.sfgpetclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ src/main/java/com/thubaas/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package com.thubaas.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import com.thubaas.sfgpetclinic.models.Visit;
+import com.thubaas.sfgpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> 
+implements VisitService {
+
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+		if(
+				visit.getPet() == null || 
+				visit.getPet().getOwner() == null || 
+				visit.getPet().getOwner().getId() == null || 
+				visit.getPet().getId() == null
+		) {
+			throw new RuntimeException("Invalid Visit");
+		}
+		return super.save(visit);
+	}
+
+	@Override
+	public void delete(Visit visit) {
+		super.delete(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
