d3154860069a1ee1b6001fab52526c2de2f50999
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2021-07-13 12:04:01 +0100

    added json endpoint for vets. closes #67

--- src/main/java/com/thubaas/sfgpetclinic/controllers/VetController.java
+++ src/main/java/com/thubaas/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.thubaas.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.thubaas.sfgpetclinic.models.Vet;
 import com.thubaas.sfgpetclinic.services.VetService;
 
 @Controller
@@ -20,6 +25,13 @@ public class VetController {
 	public String listVests(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		
+		return vetService.findAll();
+		
 	}
 
 }
